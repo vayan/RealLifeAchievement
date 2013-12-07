79a9292d787fbb6cf3b45bb654969474edac0d6b
Yann Vaillant <yann@vaillant.im>
2013-12-07 14:12:02 +0800

    first commit

---
+++ src/com/ypy/reallifeachievement/ACGroup.java
@@ -0,0 +1,10 @@
+package com.ypy.reallifeachievement;
+
+import java.util.List;
+
+public class ACGroup {
+	String id;
+	String name;
+	List<ACItem> ac;
+	
+}
---
+++ src/com/ypy/reallifeachievement/ACItem.java
@@ -0,0 +1,38 @@
+package com.ypy.reallifeachievement;
+
+import android.R.bool;
+
+public class ACItem {
+	private int weight;
+	private String name;
+	private String descr;
+	private bool done;
+	
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescr() {
+		return descr;
+	}
+	public void setDescr(String descr) {
+		this.descr = descr;
+	}
+	public bool getDone() {
+		return done;
+	}
+	public void setDone(bool done) {
+		this.done = done;
+	}
+	
+	
+	
+}	
