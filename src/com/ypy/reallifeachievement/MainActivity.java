0d032d5d8eb680ddb4a8d7c951dc8714814cba68
ScorpioEF <scorpioef@gmail.com>
2013-12-07 19:55:57 +0800

    serialization

--- src/com/ypy/reallifeachievement/MainActivity.java
+++ src/com/ypy/reallifeachievement/MainActivity.java
@@ -2,6 +2,7 @@ package com.ypy.reallifeachievement;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,15 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Utils utils = new Utils();
+		ACGroup group = new ACGroup("Success", "je suis un dieu");
+		
+		String test = utils.serialize(group);
+		Log.i("Test", test);
+		
+		ACGroup newGroup = utils.deserialize(test);
+		Log.i("Test", newGroup.getDescr());
 	}
 
 	@Override
